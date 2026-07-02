26b8d624020100d05ee61d1d9b9b1bd65c89ea1b
Deepak Maurya <deepakrmaurya@outlook.com>
2026-07-02 20:52:41 +0530

    chapter1

---
+++ app.js
@@ -0,0 +1,106 @@
+//Arithmetic Operations
+let a1 = 10;
+let b1 = 20;
+
+let add = a1 + b1;
+let subtraction = b1 - a1;
+let mul = a1 * b1;
+let div = b1 / a1;
+let remainder = b1 % a1;
+console.log(
+  `ADD: ${add} SUB: ${subtraction} MUL: ${mul} DIV: ${div} MOD: ${remainder}`,
+);
+
+//Average
+let englishMarks = 50;
+let frenchMarks = 60;
+let spanishMarks = 70;
+
+let averageMarks = (englishMarks + frenchMarks + spanishMarks) / 3;
+console.log(averageMarks);
+
+//Hours into Mins
+// let hours = prompt("enter number of hours");
+// let mins = Number(hours) * 60;
+// console.log(`${hours} hours = ${mins} mins`);
+
+//Area of rectangle
+let length = 10;
+let width = 5;
+let areaOfRectangle = length * width;
+console.log(areaOfRectangle);
+
+//square and cube of number
+// let number = prompt("Enter number to find it's Square and Cube");
+// let square = Number(number) ** 2;
+// let cube = Number(number) ** 3;
+// console.log(`For number ${number} square is ${square} and cube is ${cube}`);
+
+console.log(aa);
+var aa = 8;
+console.log(typeof aa);
+if (typeof aa === "number") {
+  console.log("right");
+}
+
+const name = "Deepak";
+let age = 24;
+console.log(`My name is ${name} and I am ${age} year old`);
+// name = "Maurya";
+age = 28;
+console.log(`now my age is ${age}`);
+
+var var1 = 10;
+console.log(var1);
+var var1 = 20;
+console.log(var1);
+
+console.log("Hello, this is from js file");
+console.log(
+  "My name is Deepak, my hobbies are: Writing, Gaming and I am 28 year old",
+);
+
+console.error("This is Error");
+console.warn("This is warning");
+
+const users = [
+  { id: 1, name: "Deepak" },
+  { id: 2, name: "Rishabh" },
+];
+console.table(users);
+
+// console.clear(); //clears the console
+
+// console.time("loop");
+
+// for (let i = 0; i < 100000; i++) {}
+
+// console.timeEnd("loop");
+
+// //Topic: Variable Intro
+// let name = "Deepak";
+// let age = 24;
+// console.log(`My name is ${name} and I am ${age} year old`);
+
+// age = 28;
+// let city = "Mumbai";
+// console.log(`My name is ${name} and now my age is ${age}, I live in ${city}`);
+
+// let x;
+// x = 100;
+// console.log(x);
+
+// //Perimeter of Rectangle
+// let length = 10;
+// let width = 5;
+// let rectanglePerimeter = 2 * (length + width);
+// console.log(rectanglePerimeter);
+
+// let movieName = "National Teasure";
+// console.log(`One of my favorite movie is ${movieName}`);
+
+// let englishMarks = 50;
+// let frenchMarks = 60;
+// let spanishMarks = 70;
+
+// console.log(`Total Score: ${englishMarks + frenchMarks + spanishMarks}`);
