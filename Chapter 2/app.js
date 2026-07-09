fba87353bb7dd30d525cded9dab6f19cad76f357
Deepak Maurya <deepakrmaurya@outlook.com>
2026-07-09 15:24:58 +0530

    Level 2 Questions solved else if

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -1,4 +1,207 @@
 //The IF, The ESLE IF and The ELSE
+
+//Topic: Level 2 – Beginner+ (Using else if)
+// Build a grading system (A, B, C, D, F).
+// const gradingSystem = (marks) => {
+//   if (marks <= 100 && marks >= 90) {
+//     console.log("A grade");
+//   } else if (marks <= 89 && marks >= 75) {
+//     console.log("B grade");
+//   } else if (marks <= 74 && marks >= 60) {
+//     console.log("C grade");
+//   } else if (marks <= 59 && marks >= 35) {
+//     console.log("D grade");
+//   } else {
+//     console.log("F grade");
+//   }
+// };
+
+// gradingSystem(100);
+// gradingSystem(90);
+// gradingSystem(89);
+// gradingSystem(34);
+// gradingSystem(76);
+// gradingSystem(45);
+// gradingSystem(62);
+
+// Find the largest of three numbers.
+// const largestOf3Numbers = (num1, num2, num3) => {
+//   if (num1 > num2 && num1 > num3) {
+//     console.log(`${num1} is greater than ${num2} and ${num3}`);
+//   } else if (num2 > num1 && num2 > num3) {
+//     console.log(`${num2} is greater than ${num1} and ${num3}`);
+//   } else {
+//     console.log(`${num3} is greater than ${num1} and ${num2}`);
+//   }
+// };
+
+// largestOf3Numbers(1, 2, 3);
+// largestOf3Numbers(30, 22, 11);
+// largestOf3Numbers(-34, -88, -123);
+
+// Check if a number is positive, negative, or zero.
+// const isNumber = (number) => {
+//   if (number > 0) {
+//     console.log("Positive");
+//   } else if (number < 0) {
+//     console.log("Negative");
+//   } else {
+//     console.log("Zero");
+//   }
+// };
+
+// isNumber(-66);
+// isNumber(98);
+// isNumber(0);
+
+// Create a simple calculator using two numbers and an operator (+, -, *, /).
+// const simpleCalculator = (operator) => {
+//   let num1 = 10;
+//   let num2 = 5;
+//   if (operator === "Add") {
+//     console.log(num1 + num2);
+//   } else if (operator === "Sub") {
+//     console.log(num1 - num2);
+//   } else if (operator === "Mul") {
+//     console.log(num1 * num2);
+//   } else if (operator === "Div") {
+//     console.log(num1 / num2);
+//   } else {
+//     console.log("Please enter a valid operator name");
+//   }
+// };
+
+// simpleCalculator("Add");
+// simpleCalculator("Sub");
+// simpleCalculator("Mul");
+// simpleCalculator("Div");
+// simpleCalculator("Mod");
+
+// Determine the season based on the month number.
+// const season = (month) => {
+//   if (
+//     month === "January" ||
+//     month === "October" ||
+//     month === "November" ||
+//     month === "December"
+//   ) {
+//     console.log("Winter");
+//   } else if (
+//     month === "February" ||
+//     month === "March" ||
+//     month === "April" ||
+//     month === "May"
+//   ) {
+//     console.log("Summer");
+//   } else if (
+//     month === "June" ||
+//     month === "July" ||
+//     month === "August" ||
+//     month === "September"
+//   ) {
+//     console.log("Rainy");
+//   } else {
+//     console.log("Please Enter a Valid Month Name");
+//   }
+// };
+// season("May");
+// season("October");
+// season("June");
+// season("Monday");
+
+// Print the day type (Weekday or Weekend) from the day name.
+// const dayType = (day) => {
+//   if (
+//     day === "Monday" ||
+//     day === "Tuesday" ||
+//     day === "Wednesday" ||
+//     day === "Thursday" ||
+//     day === "Friday"
+//   ) {
+//     console.log("It's a Week Day");
+//   } else if (day === "Sunday" || day === "Saturday") {
+//     console.log("It's a Weekend");
+//   } else {
+//     console.log("Please enter a valid Day");
+//   }
+// };
+
+// dayType("Monday");
+// dayType("Sunday");
+// dayType("Wednesday");
+// dayType("April");
+
+// Check BMI category (Underweight, Normal, Overweight, Obese).
+// const bmiCalculator = (bmi) => {
+//   if (bmi >= 30) {
+//     console.log("Obese");
+//   } else if (bmi <= 29.9 && bmi >= 25) {
+//     console.log("OverWeight");
+//   } else if (bmi <= 24.9 && bmi >= 18.5) {
+//     console.log("Normal");
+//   } else if (bmi <= 18.4) {
+//     console.log("UnderWeight");
+//   } else {
+//     console.log("Please Enter a Valid Number");
+//   }
+// };
+
+// bmiCalculator(50);
+// bmiCalculator(12);
+// bmiCalculator(19);
+// bmiCalculator(28);
+// bmiCalculator("asd");
+
+// Check ticket price based on age: Child, Adult, Senior Citizen
+// function ticketPrices(age) {
+//   if (age >= 60) {
+//     console.log("for Senior Citizen  Ticket Price: 25");
+//   } else if (age <= 59 && age >= 15) {
+//     console.log("for Adult  Ticket Price: 40");
+//   } else if (age <= 14) {
+//     console.log("for Child  Ticket Price: 20");
+//   } else {
+//     console.log("Please enter a valid age");
+//   }
+// }
+// ticketPrices(60);
+// ticketPrices(9);
+// ticketPrices("Thirty");
+// ticketPrices(33);
+
+// Assign shipping charges based on order amount.
+// const shippingCharges = (cartAmount) => {
+//   if (cartAmount >= 500) {
+//     console.log("Free Shipping");
+//   } else if (cartAmount < 500) {
+//     console.log("Shipping Charges: Rs.100");
+//   } else {
+//     console.error("Error");
+//   }
+// };
+
+// shippingCharges(501);
+// shippingCharges(250);
+// shippingCharges("Four");
+
+// Check internet speed: Slow, Medium, Fast
+// const internetSpeed = (speed) => {
+//   if (speed > 100) {
+//     console.log("Internet Speed: Fast");
+//   } else if (speed <= 99 && speed >= 40) {
+//     console.log("Internet Speed: Medium");
+//   } else if (speed <= 39 && speed >= 1) {
+//     console.log("Internet Speed: Slow");
+//   } else {
+//     console.log("Please enter valid speed number");
+//   }
+// };
+// internetSpeed(102);
+// internetSpeed(60);
+// internetSpeed(20);
+// internetSpeed(-100);
+
+//Topic: Level 1 – Beginner (Easy)
 // Check if a number is positive or negative.
 // const numberIs = (number) => {
 //   if (number > 0) {
